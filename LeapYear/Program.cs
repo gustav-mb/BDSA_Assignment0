95d08f506b6446dc0ceca7829472aafe5f51575d
Gustav12dk00 <laugus11@hotmail.com>
2021-09-09 18:59:31 +0200

    Removed old leap year method

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -20,14 +20,6 @@ namespace LeapYear
                 Console.WriteLine(e.Message);
             }
         }
-
-        public static bool IsLeapYear(int year)
-        {
-            if (year == 0) return false;
-            if (year % 4 == 0 && year % 100 != 0) return true;
-            else if (year % 400 == 0) return true;
-            return false;
-        }
     }
 
     public class LeapYearChecker 
